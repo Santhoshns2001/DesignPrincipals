469b875fa00d31ada76aaa7b0096af61287cfa31
Santhuns2001 <n.s.santhosh116@gmail.com>
2024-04-22 12:37:00 +0530

    initail commit

---
+++ AreaCaluculator/AreaCaluculate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPrincipalsImpliDemo.AreaCaluculator
+{
+    public class AreaCaluculate
+    {
+        public static double AreaCal(Shape[] arrShapes ) {
+
+            double area = 0;
+            foreach(var objShape in arrShapes)
+            {
+                area += objShape.Area();
+            }
+            return area;
+        }
+    }
+} 
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.SqlClient;
+
+namespace DesignPrincipalsImpliDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello, World!");
+
+            SqlConnection sqlConnect;
+            string connectingString = @"Data Source=DELL\SQLEXPRESS;Initial Catalog=FirstDatabase;Integrated Security=True;Trust Server Certificate=True";
+
+            try
+            {
+                sqlConnect = new SqlConnection(connectingString);
+                sqlConnect.Open();
+                Console.WriteLine("connected");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
